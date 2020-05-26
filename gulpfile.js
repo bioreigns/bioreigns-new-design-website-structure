8048e1d90107b00d3d4f11f021eed0613b951ca7
Ji20on <lmarkjason.20@gmail.com>
2020-05-27 06:15:50 +0800

    ongoing fixing the responsive of all pages

--- gulpfile.js
+++ gulpfile.js
@@ -7,11 +7,25 @@ const browserSync = require('browser-sync').create();
 //compile scss into css
 function style() {
     //1. where is my scss file
-    return gulp.src('./scss/main.scss')
+    return gulp.src('./scss/*.scss')
         //2. pass that data file through sass compiler
         .pipe(sass())
         //where  do i save the compiled css
         .pipe(gulp.dest('./dist/css'))
 }
 
+function watch() {
+    browserSync.init({
+        server: {
+            baseDir: './'
+        }
+    });
+
+    gulp.watch('./scss/*.scss', style);
+    gulp.watch('./*.html').on('change', browserSync.reload);
+    gulp.watch('./dist/**/*.js').on('change', browserSync.reload);
+}
+
+
 exports.style = style;
+exports.watch = watch;
